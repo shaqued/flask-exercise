9d09568b595d7984a7d8b53c5c30284cffba0405
Shaqued <shaqued201@gmail.com>
2021-03-29 19:49:23 +0300

    add part 1->6

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def create_response(
     data: dict = None, status: int = 200, message: str = ""
 ) -> Tuple[Response, int]:
     """Wraps response in a consistent format throughout the API.
-    
+
     Format inspired by https://medium.com/@shazow/how-i-design-json-api-responses-71900f00f2db
     Modifications included:
     - make success a boolean since there's only 2 values
@@ -52,7 +52,57 @@ def mirror(name):
     return create_response(data)
 
 
-# TODO: Implement the rest of the API here!
+@app.route("/users")
+def get_users():
+    data = db.get('users')
+
+    if "team" in request.args:
+        data = list(filter(lambda x: x["team"] == request.args["team"], data))
+
+    return create_response({"users": data})
+
+
+@app.route("/users/<id>")
+def get_user(id):
+    data = db.getById('users', int(id))
+
+    if data is None:
+        return create_response(status=404, message="user cannot be found")
+
+    return create_response(data)
+
+
+@app.route('/users', methods=['POST'])
+def post():
+    # TODO: check existing params
+    def verify_params():
+        pass
+
+    data = request.json
+    verify_params()
+    new_user = db.create('users', data)
+
+    return create_response(new_user)
+
+@app.route('/users/<id>', methods=['PUT'])
+def put(id):
+    data = request.json
+    keys = ['name', 'age', 'team']
+    props = dict((k, data[k]) for k in keys if k in data)
+    updated_user = db.updateById('users', int(id), props)
+
+    if(updated_user is None):
+        return create_response(status=404, message="user to update cannot be found")
+    return create_response(updated_user)
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete(id):
+    user = db.getById('users', int(id))
+
+    if(user is None):
+        return create_response(status=404, message="user to delete cannot be found")
+    db.deleteById('users', int(id))
+    return create_response(status=200, message="deleted user successfully")
 
 """
 ~~~~~~~~~~~~ END API ~~~~~~~~~~~~
